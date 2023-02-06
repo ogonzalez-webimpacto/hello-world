0d95dea4b1afcc03bd183d3bd1c612f6ad9743ca
ogonzalez-webimpacto <ogonzalez@webimpacto.es>
2023-02-06 09:59:12 -0600

    Update FormWorkWithUs.jsx

--- react/components/Forms/FormWorkWithUs.jsx
+++ react/components/Forms/FormWorkWithUs.jsx
@@ -1,16 +1,60 @@
 import React from 'react';
 import '../../css/index.css'
+import { useState } from 'react';
 
 function FormWorkWithUs(){
+  const [nombre,setNombre] = useState="";
+  const [apellidos,setApellidos] = useState="";
+  const [email,setEmail]= useState="";
+  const [poblacion,setPoblacion] = useState="";
+  const [provincia,setProvincia]= useState="";
+  const [cp,setCP]= useState="";
+  const [tel1,setTel1] = useState="";
+  const [tel2,setTel2] =useState ="";
+  const [message, setMessage] = useState("");
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await fetch("https://webimpacto.vtexcommercestable.com.br/dataentities/:dataEntityName/documents?_schema=candidatesInfo", {
+        method: "POST",
+        body: JSON.stringify({
+          nombre: nombre,
+          apellidos: apellidos,
+          poblacion: poblacion,
+          provincia: provincia,
+          codigo_postal: cp,
+          tel1: tel1,
+          tel2: tel2,
+          email: email
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 201) {
+        setName("");
+        setApellidos("");
+        setEmail("");
+        setMessage("Información cargada");
+      } else {
+        setMessage("Some error occured");
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
   return(
     <>
     <div className="formbold-main-wrapper">
       <div className="formbold-form-wrapper">
-        <form action="">
+        <form onSubmit={handleSubmit}>
+        <div className="message">{message ? <p>{message}</p> : null}</div>
           <div className="formbold-mb-5">
             <label for="nombre" className="formbold-form-label"> Nombre </label>
             <input
               type="text"
+              value={nombre}
               name="nombre"
               id="nombre"
               placeholder="Nombre"
@@ -21,6 +65,7 @@ function FormWorkWithUs(){
             <label for="apellidos" className="formbold-form-label"> Apellidos </label>
             <input
               type="text"
+              value={apellidos}
               name="apellidos"
               id="apellidos"
               placeholder="Apellidos"
@@ -32,6 +77,7 @@ function FormWorkWithUs(){
             <label for="email" className="formbold-form-label"> Email </label>
             <input
               type="email"
+              value={email}
               name="email"
               id="email"
               placeholder="Correo electrónico"
